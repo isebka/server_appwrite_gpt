16cbe5f1d7615840824d05972b02e46e232f87eb
isebka <alekseyyyrevich@mail.ru>
2025-09-27 18:19:52 +0300

    hhh

--- src/main.py
+++ src/main.py
@@ -87,7 +87,7 @@ async def main(context):
 
     if context.req.method == "POST" and context.req.path == "/add_email":
         try:
-        	logger.info("test")
+            logger.info("test")
             data = context.req.body  
         
             # Проверяем, является ли data словарем, как ожидается
